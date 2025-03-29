b1af0e0c474487f0640823782069a9b8d7668fb4
Florian <florian.carriere@gmail.com>
2025-03-29 19:11:50 +0100

    clean

--- database/db-manager.js
+++
@@ -1,8 +0,0 @@
-/**
- * Module de base de données supprimé
- * Les fonctionnalités de sauvegarde ne sont plus requises
- */
-
-module.exports = {
-  // Interface vide pour maintenir la compatibilité
-};
